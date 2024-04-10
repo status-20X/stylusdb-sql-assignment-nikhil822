f640299a253d953d6ae5462a77ade23defd2fc49
nikhil822 <nikhilsahu1312@gmail.com>
2024-04-10 21:15:48 +0530

    feat: steps 2

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,11 +1,9 @@
-const executeSELECTQuery = require("../src/index");
+const readCSV = require("../src/csvReader");
 
-test("Execute SQL Query", async () => {
-  const query = "SELECT id, name FROM sample";
-  const result = await executeSELECTQuery(query);
-  expect(result.length).toBeGreaterThan(0);
-  expect(result[0]).toHaveProperty("id");
-  expect(result[0]).toHaveProperty("name");
-  expect(result[0]).not.toHaveProperty("age");
-  expect(result[0]).toEqual({ id: "1", name: "John" });
+test("Read CSV File", async () => {
+  const data = await readCSV("./sample.csv");
+  expect(data.length).toBeGreaterThan(0);
+  expect(data.length).toBe(3);
+  expect(data[0].name).toBe("John");
+  expect(data[0].age).toBe("30"); //ignore the string type here, we will fix this later
 });
